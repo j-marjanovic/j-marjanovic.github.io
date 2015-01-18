954d5bf23acc7f51febd384566a557232eecd187
Jan Marjanovic <jancooo@gmail.com>
2015-01-18 23:26:06 +0100

    Added space in tag list

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,7 +13,10 @@ TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+#FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
